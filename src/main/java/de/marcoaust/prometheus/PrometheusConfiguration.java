71ad751b94e9d51fa396a433b2528166a0108730
Eliezio Oliveira <eliezio.oliveira@riocard.com>
2016-09-11 21:06:36 -0300

    added conditional configuration of simpleclient_hotspot

--- src/main/java/de/marcoaust/prometheus/PrometheusConfiguration.java
+++ src/main/java/de/marcoaust/prometheus/PrometheusConfiguration.java
@@ -3,8 +3,12 @@ package de.marcoaust.prometheus;
 import java.util.Collection;
 
 import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.hotspot.DefaultExports;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 class PrometheusConfiguration {
   private CollectorRegistry collectorRegistry = CollectorRegistry.defaultRegistry;
@@ -15,7 +19,16 @@ class PrometheusConfiguration {
     springBootMetricsCollector.register(collectorRegistry);
     return springBootMetricsCollector;
   }
+
+  @Configuration
+  @ConditionalOnClass(DefaultExports.class)
+  @ConditionalOnProperty(prefix = "prometheus.hotspot", name = "enabled", matchIfMissing = true)
+  static class HotspotMetricsExporter {
+    public HotspotMetricsExporter() {
+      DefaultExports.initialize();
     }
+  }
+
   @Bean
   public PrometheusEndpoint prometheusEndpoint() {
     return new PrometheusEndpoint(collectorRegistry);
