c3210efc4d8f616b7395ed28c89f29cda947e48f
Eliezio Oliveira <eliezio.oliveira@riocard.com>
2016-09-11 21:05:56 -0300

    beans instantiated explicitly

--- src/main/java/de/marcoaust/prometheus/PrometheusConfiguration.java
+++ src/main/java/de/marcoaust/prometheus/PrometheusConfiguration.java
@@ -2,16 +2,22 @@ package de.marcoaust.prometheus;
 
 import java.util.Collection;
 
+import io.prometheus.client.CollectorRegistry;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("de.marcoaust.prometheus")
 class PrometheusConfiguration {
-    @Bean
-    public SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
-        SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
-        springBootMetricsCollector.register();
-        return springBootMetricsCollector;
+  private CollectorRegistry collectorRegistry = CollectorRegistry.defaultRegistry;
+
+  @Bean
+  public SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
+    SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
+    springBootMetricsCollector.register(collectorRegistry);
+    return springBootMetricsCollector;
+  }
     }
+  @Bean
+  public PrometheusEndpoint prometheusEndpoint() {
+    return new PrometheusEndpoint(collectorRegistry);
+  }
 }
